e9883e878a0d9d7e7fe17e8a544b0d2cdd3fe8e7
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-10-09 22:57:14 +0300

    2666. Allow One Function Call

---
+++ 2666. Allow One Function Call/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+    return function(...args){
+        if (called) return undefined;
+        called = true;
+        return fn(...args);
+    }
+};
+
+let fn = (a,b,c) => (a + b + c)
+let onceFn = once(fn)
+
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(1,2,3)); // returns undefined without calling fn
