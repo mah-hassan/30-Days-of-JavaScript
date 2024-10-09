34199f8d89c49ce91158e80a720bee0d6edbaf27
Mahmoud Hassan <113454774+mah-hassan@users.noreply.github.com>
2024-10-09 22:36:09 +0300

    2629. Function Composition #day8

---
+++ 2629. Function Composition/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    const fun = (n, f) => f(n);
+    return function(x) {
+        return functions.reduceRight(fun, x);
+    }
+    
+    /* Alternative solution using a loop */
+/*     return function(x) {
+        for(let i = functions.length - 1; i >= 0; i--) {
+            x = functions[i](x);
+        }
+        return x;
+    } */
+};
+
+
+const fn = compose([x => x + 1, x => 2 * x])
+console.log(fn(4)); // 9
